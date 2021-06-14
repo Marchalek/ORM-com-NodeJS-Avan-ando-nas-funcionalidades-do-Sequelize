407bec71b472ae23a54deb3f77f3f7103fdc8374
Marchalek <marchalek@hotmail.com>
2021-06-14 10:40:21 -0300

    M2 terminado

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,11 +1,37 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Pessoas = sequelize.define('Pessoas', {
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function(dado) {
+          if (dado.length < 3) {
+            throw new Error ('O campo nome deve ter mais de 3 caracteres')
+          }
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'dado do tipo email invalidos'
+        }
+      }
+    },
     role: DataTypes.STRING
-  }, { paranoid: true })
+  }, { 
+    paranoid: true,
+    defaultScope: {
+      where: { ativo: true}
+    },
+    scopes: {
+      todos: {where: {}},
+      //etc: { constraints }
+    }
+  })
   Pessoas.associate = function(models) {
     Pessoas.hasMany(models.Turmas, {
       foreignKey: 'docente_id'
